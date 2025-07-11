60791bc5ff820117ff17dac5e3c81d4e86615438
flamurshala <flamur8600@gmail.com>
2025-07-11 16:44:52 +0200

    edit how data is shown to be compatible with the database changes

--- database/e-learning.sql
+++ database/e-learning.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 08, 2025 at 12:53 PM
+-- Generation Time: Jul 11, 2025 at 02:44 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `e-learning`
+-- Database: `tsms`
 --
 
 -- --------------------------------------------------------
@@ -52,6 +52,13 @@ CREATE TABLE `admin_notifications` (
   `message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admin_notifications`
+--
+
+INSERT INTO `admin_notifications` (`id`, `message`, `created_at`) VALUES
+(24, '📝 Make the certificates ready for course \"test notifi 2\". Only two session remains.', '2025-07-11 14:03:23');
 
 -- --------------------------------------------------------
 
@@ -117,11 +124,39 @@ CREATE TABLE `attendance` (
 --
 
 INSERT INTO `attendance` (`id`, `session_id`, `student_id`, `status`, `updated_at`) VALUES
-(284, 28, 30, 'present', '2025-07-08 12:02:20'),
-(285, 29, 30, 'present', '2025-07-08 12:02:28'),
 (286, 30, 30, 'present', '2025-07-08 12:02:37'),
 (287, 32, 30, 'absent', '2025-07-08 12:38:11'),
-(288, 32, 32, 'present', '2025-07-08 12:38:11');
+(288, 32, 32, 'present', '2025-07-08 12:38:11'),
+(289, 27, 30, 'present', '2025-07-10 11:50:42'),
+(290, 27, 31, 'present', '2025-07-10 11:50:42'),
+(293, 28, 30, 'present', '2025-07-10 12:01:10'),
+(294, 28, 31, 'present', '2025-07-10 12:01:10'),
+(295, 29, 30, 'present', '2025-07-10 12:02:53'),
+(296, 29, 31, 'present', '2025-07-10 12:02:53'),
+(300, 40, 30, 'absent', '2025-07-10 12:41:25'),
+(301, 40, 31, 'present', '2025-07-10 12:41:25'),
+(302, 40, 32, 'present', '2025-07-10 12:41:25'),
+(303, 41, 30, 'present', '2025-07-10 12:41:33'),
+(304, 41, 31, 'present', '2025-07-10 12:41:33'),
+(305, 52, 30, 'absent', '2025-07-10 12:43:59'),
+(306, 52, 31, 'present', '2025-07-10 12:43:59'),
+(307, 52, 32, 'present', '2025-07-10 12:43:59'),
+(308, 49, 30, 'present', '2025-07-10 12:44:15'),
+(309, 49, 31, 'present', '2025-07-10 12:44:15'),
+(310, 49, 32, 'online', '2025-07-10 12:44:15'),
+(311, 50, 30, 'present', '2025-07-10 12:44:24'),
+(312, 50, 31, 'present', '2025-07-10 12:44:24'),
+(313, 50, 32, 'present', '2025-07-10 12:44:24'),
+(314, 51, 30, 'present', '2025-07-10 12:44:32'),
+(315, 51, 31, 'present', '2025-07-10 12:44:32'),
+(316, 51, 32, 'present', '2025-07-10 12:44:32'),
+(317, 39, 30, 'present', '2025-07-11 11:49:54'),
+(318, 39, 31, 'present', '2025-07-11 11:49:54'),
+(319, 39, 32, 'present', '2025-07-11 11:49:54'),
+(320, 55, 33, 'present', '2025-07-11 11:51:44'),
+(321, 56, 33, 'present', '2025-07-11 11:51:52'),
+(322, 60, 30, 'present', '2025-07-11 12:03:07'),
+(323, 61, 30, 'present', '2025-07-11 12:03:23');
 
 -- --------------------------------------------------------
 
@@ -144,7 +179,11 @@ CREATE TABLE `courses` (
 
 INSERT INTO `courses` (`id`, `title`, `description`, `created_at`, `professor_id`, `completed`) VALUES
 (44, 'Full Stack', 'test', '2025-07-08 12:00:29', 11, 1),
-(45, 'Python 2', 'test', '2025-07-08 12:34:00', 11, 0);
+(45, 'Python 2', 'test', '2025-07-08 12:34:00', 11, 0),
+(46, 'testim', 'adsdasdasd', '2025-07-10 12:40:35', 9, 0),
+(48, 'me 4', 'dadasd', '2025-07-10 12:43:07', 9, 1),
+(49, 'test notifications', 'testimi a po bajn notifications', '2025-07-11 11:51:17', 9, 0),
+(50, 'test notifi 2', 'qdqw', '2025-07-11 12:02:33', 9, 0);
 
 -- --------------------------------------------------------
 
@@ -165,7 +204,11 @@ CREATE TABLE `course_professor` (
 
 INSERT INTO `course_professor` (`id`, `professor_id`, `course_id`, `enrolled_at`) VALUES
 (30, 11, 44, '2025-07-08 14:00:29'),
-(31, 11, 45, '2025-07-08 14:34:00');
+(31, 11, 45, '2025-07-08 14:34:00'),
+(32, 9, 46, '2025-07-10 14:40:35'),
+(34, 9, 48, '2025-07-10 14:43:07'),
+(35, 9, 49, '2025-07-11 13:51:17'),
+(36, 9, 50, '2025-07-11 14:02:33');
 
 -- --------------------------------------------------------
 
@@ -188,7 +231,18 @@ INSERT INTO `course_student` (`id`, `student_id`, `course_id`, `enrolled_at`) VA
 (92, 30, 44, '2025-07-08 12:01:01'),
 (93, 31, 44, '2025-07-08 12:01:31'),
 (94, 30, 45, '2025-07-08 12:34:00'),
-(95, 32, 45, '2025-07-08 12:34:35');
+(95, 32, 45, '2025-07-08 12:34:35'),
+(96, 30, 46, '2025-07-10 12:40:35'),
+(97, 31, 46, '2025-07-10 12:40:35'),
+(98, 32, 46, '2025-07-10 12:40:35'),
+(102, 30, 48, '2025-07-10 12:43:07'),
+(103, 31, 48, '2025-07-10 12:43:07'),
+(104, 32, 48, '2025-07-10 12:43:07'),
+(106, 33, 44, '2025-07-10 13:39:24'),
+(107, 33, 49, '2025-07-11 11:51:17'),
+(108, 30, 50, '2025-07-11 12:02:33'),
+(109, 34, 44, '2025-07-11 12:25:57'),
+(110, 34, 45, '2025-07-11 12:25:57');
 
 -- --------------------------------------------------------
 
@@ -211,7 +265,8 @@ CREATE TABLE `professors` (
 INSERT INTO `professors` (`id`, `name`, `email`, `created_at`, `password`) VALUES
 (9, 'Mergim Alidema', 'mergim@gmail.com', '2025-06-20 08:16:13', '1234'),
 (10, 'Egzon Uka', 'egzon@gmail.com', '2025-06-20 08:16:37', '4321'),
-(11, 'Erion Prokshi', 'erion@gmail.com', '2025-06-22 15:17:15', '1234');
+(11, 'Erion Prokshi', 'erion@gmail.com', '2025-06-22 15:17:15', '1234'),
+(12, 'new prof', 'newprof@gmail.com', '2025-07-11 12:31:37', '1234');
 
 -- --------------------------------------------------------
 
@@ -280,19 +335,23 @@ INSERT INTO `replies` (`id`, `question_id`, `user`, `reply`, `created_at`) VALUE
 CREATE TABLE `students` (
   `id` int NOT NULL,
   `name` varchar(100) NOT NULL,
+  `surname` varchar(100) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `phone_number` varchar(20) DEFAULT NULL
+  `notes` text,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `personal_number` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `name`, `email`, `password`, `phone_number`) VALUES
-(30, 'Omer Mulliqi', 'omer@gmail.com', '123', '045 757 823'),
-(31, 'Alaudn haradinaj', 'alaudin@gamil.com', '123', '044 638 823'),
-(32, 'Jakub Demalia', 'jakub@gmail.com', '123', '253525235');
+INSERT INTO `students` (`id`, `name`, `surname`, `email`, `notes`, `phone_number`, `personal_number`) VALUES
+(30, 'Omer Mulliqi', NULL, 'omer@gmail.com', NULL, '045 757 823', NULL),
+(31, 'Alaudn haradinaj', NULL, 'alaudin@gamil.com', NULL, '044 638 823', NULL),
+(32, 'Jakub Demalia', NULL, 'jakub@gmail.com', NULL, '253525235', NULL),
+(33, 'Erion Prokshi', NULL, 'erion-prokshi@hotmail.com', NULL, '63131116151', NULL),
+(34, 'Erion', 'shala', 'erion@hotmail.com', 'dadasdsadsadasd', '63131116151', '1240887033');
 
 -- --------------------------------------------------------
 
@@ -334,9 +393,12 @@ CREATE TABLE `student_payments` (
 --
 
 INSERT INTO `student_payments` (`id`, `student_id`, `course_id`, `payment_method`, `amount_all`, `amount_month1`, `amount_month2`, `created_at`) VALUES
-(6, 30, 44, 'All', '100.00', NULL, NULL, '2025-07-08 12:01:01'),
-(7, 31, 44, 'Divided', NULL, '50.00', '50.00', '2025-07-08 12:01:31'),
-(8, 32, 45, 'All', '100.00', NULL, NULL, '2025-07-08 12:34:35');
+(6, 30, 44, 'All', 100.00, NULL, NULL, '2025-07-08 12:01:01'),
+(7, 31, 44, 'Divided', NULL, 50.00, 50.00, '2025-07-08 12:01:31'),
+(8, 32, 45, 'All', 100.00, NULL, NULL, '2025-07-08 12:34:35'),
+(10, 33, 44, 'All', 270.00, NULL, NULL, '2025-07-10 13:39:24'),
+(11, 34, 44, 'All', 250.00, NULL, NULL, '2025-07-11 12:25:57'),
+(12, 34, 45, 'Divided', NULL, 100.00, 150.00, '2025-07-11 12:25:57');
 
 -- --------------------------------------------------------
 
@@ -373,9 +435,9 @@ CREATE TABLE `training_sessions` (
 --
 
 INSERT INTO `training_sessions` (`id`, `course_id`, `session_number`, `session_date`, `created_at`, `submitted_at`, `submitted_after_seconds`) VALUES
-(27, 44, 1, '2025-07-08 12:00:29', '2025-07-08 12:00:29', '2025-07-08 14:02:08', 0),
-(28, 44, 2, '2025-07-09 12:00:29', '2025-07-08 12:00:29', '2025-07-08 14:02:20', 0),
-(29, 44, 3, '2025-07-10 12:00:29', '2025-07-08 12:00:29', '2025-07-08 14:02:28', 0),
+(27, 44, 1, '2025-07-08 12:00:29', '2025-07-08 12:00:29', '2025-07-10 13:50:42', 0),
+(28, 44, 2, '2025-07-09 12:00:29', '2025-07-08 12:00:29', '2025-07-10 14:01:10', 0),
+(29, 44, 3, '2025-07-10 12:00:29', '2025-07-08 12:00:29', '2025-07-10 14:02:53', 0),
 (30, 44, 4, '2025-07-11 12:00:29', '2025-07-08 12:00:29', '2025-07-08 14:02:37', 0),
 (31, 44, 5, '2025-07-12 12:00:29', '2025-07-08 12:00:29', NULL, NULL),
 (32, 45, 1, '2025-07-08 12:34:00', '2025-07-08 12:34:00', '2025-07-08 14:38:11', 0),
@@ -384,7 +446,28 @@ INSERT INTO `training_sessions` (`id`, `course_id`, `session_number`, `session_d
 (35, 45, 4, '2025-07-11 12:34:00', '2025-07-08 12:34:00', NULL, NULL),
 (36, 45, 5, '2025-07-12 12:34:00', '2025-07-08 12:34:00', NULL, NULL),
 (37, 45, 6, '2025-07-13 12:34:00', '2025-07-08 12:34:00', NULL, NULL),
-(38, 45, 7, '2025-07-14 12:34:00', '2025-07-08 12:34:00', NULL, NULL);
+(38, 45, 7, '2025-07-14 12:34:00', '2025-07-08 12:34:00', NULL, NULL),
+(39, 46, 1, '2025-07-10 12:40:35', '2025-07-10 12:40:35', '2025-07-11 13:49:54', 0),
+(40, 46, 2, '2025-07-11 12:40:35', '2025-07-10 12:40:35', '2025-07-10 14:41:25', 0),
+(41, 46, 3, '2025-07-12 12:40:35', '2025-07-10 12:40:35', '2025-07-10 14:41:33', 0),
+(42, 46, 4, '2025-07-13 12:40:35', '2025-07-10 12:40:35', NULL, NULL),
+(43, 46, 5, '2025-07-14 12:40:35', '2025-07-10 12:40:35', NULL, NULL),
+(49, 48, 1, '2025-07-10 12:43:07', '2025-07-10 12:43:07', '2025-07-10 14:44:15', 0),
+(50, 48, 2, '2025-07-11 12:43:07', '2025-07-10 12:43:07', '2025-07-10 14:44:24', 0),
+(51, 48, 3, '2025-07-12 12:43:07', '2025-07-10 12:43:07', '2025-07-10 14:44:32', 0),
+(52, 48, 4, '2025-07-13 12:43:07', '2025-07-10 12:43:07', '2025-07-10 14:43:59', 0),
+(53, 48, 5, '2025-07-14 12:43:07', '2025-07-10 12:43:07', NULL, NULL),
+(54, 48, 6, '2025-07-15 12:43:07', '2025-07-10 12:43:07', NULL, NULL),
+(55, 49, 1, '2025-07-11 11:51:17', '2025-07-11 11:51:17', '2025-07-11 13:51:44', 0),
+(56, 49, 2, '2025-07-12 11:51:17', '2025-07-11 11:51:17', '2025-07-11 13:51:52', 0),
+(57, 49, 3, '2025-07-13 11:51:17', '2025-07-11 11:51:17', NULL, NULL),
+(58, 49, 4, '2025-07-14 11:51:17', '2025-07-11 11:51:17', NULL, NULL),
+(59, 49, 5, '2025-07-15 11:51:17', '2025-07-11 11:51:17', NULL, NULL),
+(60, 50, 1, '2025-07-11 12:02:33', '2025-07-11 12:02:33', '2025-07-11 14:03:07', 0),
+(61, 50, 2, '2025-07-12 12:02:33', '2025-07-11 12:02:33', '2025-07-11 14:03:23', 0),
+(62, 50, 3, '2025-07-13 12:02:33', '2025-07-11 12:02:33', NULL, NULL),
+(63, 50, 4, '2025-07-14 12:02:33', '2025-07-11 12:02:33', NULL, NULL),
+(64, 50, 5, '2025-07-15 12:02:33', '2025-07-11 12:02:33', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -578,13 +661,13 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `admin_notifications`
 --
 ALTER TABLE `admin_notifications`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `announcements`
 --
 ALTER TABLE `announcements`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `assignments`
@@ -596,31 +679,31 @@ ALTER TABLE `assignments`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=289;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=324;
 
 --
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `course_professor`
 --
 ALTER TABLE `course_professor`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `course_student`
 --
 ALTER TABLE `course_student`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
 
 --
 -- AUTO_INCREMENT for table `professors`
 --
 ALTER TABLE `professors`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `questions`
@@ -638,7 +721,7 @@ ALTER TABLE `replies`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `student_course_payments`
@@ -650,7 +733,7 @@ ALTER TABLE `student_course_payments`
 -- AUTO_INCREMENT for table `student_payments`
 --
 ALTER TABLE `student_payments`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `student_progress`
@@ -662,7 +745,7 @@ ALTER TABLE `student_progress`
 -- AUTO_INCREMENT for table `training_sessions`
 --
 ALTER TABLE `training_sessions`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `user_announcements`
